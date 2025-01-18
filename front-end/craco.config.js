febd129941b60eae41dc9ecd74e114cd97aff723
Felipe Pereira dos Santos <fpsantos86@hotmail.com>
2025-01-18 20:06:50 -0300

    Changes to be committed: 	modified:   .github/workflows/docker-publish.yml 	modified:   front-end/Dockerfile.front_end 	modified:   front-end/craco.config.js 	modified:   front-end/package.json 	modified:   front-end/yarn.lock

--- front-end/craco.config.js
+++ front-end/craco.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     webpack: {
       configure: (webpackConfig) => {
+        console.log("Configurações de Webpack antes:", webpackConfig.plugins);
         webpackConfig.plugins = webpackConfig.plugins.filter(
           (plugin) => plugin.constructor.name !== "ForkTsCheckerWebpackPlugin"
         );
+        console.log("Configurações de Webpack depois:", webpackConfig.plugins);
         return webpackConfig;
       },
     },
